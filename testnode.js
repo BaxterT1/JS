6a2ceb610648ca76132865cd6f666c36aae1016a
Ali <ali.mtarar@gmail.com>
2024-07-18 20:20:21 +1000

    if, else

--- testnode.js
+++ testnode.js
@@ -1,5 +1,11 @@
-var a;
-let b;
+var a = 13;
+
+var b = 56;
 
 console.log("a = " + a);
 console.log("b = " + b);
+
+if (a == 12 || b > 50) console.log("condition a == 12 || b > 50 is true")
+
+else console.log("condition a ++ 12 || b > 50 is false");
+
